929f6d6f2ef3b7c0b064a47334d2acd101545bec
martianbug <bichosiete@hotmail.com>
2023-03-05 20:58:26 +0100

    commit from raspy

--- osc.py
+++ osc.py
@@ -1,14 +1,18 @@
 from pythonosc import osc_server
+from pythonosc.dispatcher import Dispatcher
 
 global humidity
 global temp
 
-def get_data(address, *args):
+def get_data(address, *args, temp):
     # global temp
     # global humidity
     # temp = args[0]
     # humidity = args[2]
-    print(f"{address}: {args}")
+    try:
+        print(temp)
+        print(f"{address}: {args}")
+    except ValueError: pass
 
 if __name__ == "__main__":
     # Parse command line arguments
@@ -16,13 +20,14 @@ if __name__ == "__main__":
     # parser.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
     # parser.add_argument("--port", type=int, default=5005, help="The port to listen on")
     # args = parser.parse_args()
-    ip ="192.168.1.18"
-    port = 9999
+    ip ="192.168.1.20"
+    port = 5005
+    dispatcher = Dispatcher()
+    dispatcher.map("/data", get_data, "temp")
     
-    server = osc_server.ThreadingOSCUDPServer((ip, port), osc_server.Dispatcher())
+    server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
     print(f"Serving on {server.server_address}")
     # server.dispatcher.map("/test", print_handler)
-    server.dispatcher.map("/data", get_data)
     # msg = server.get_request()
     server.serve_forever()
     
