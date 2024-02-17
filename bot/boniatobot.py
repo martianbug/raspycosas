cc84849472684cfa3b2f3090543691376985f50c
bichomartiano <bichosiete@hotmail.com>
2024-02-17 14:46:22 +0100

    fixes

--- bot/boniatobot.py
+++ bot/boniatobot.py
@@ -65,14 +65,14 @@ async def spotify(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     #     await update.message.reply_text(f'Pero qué digo???')
     #     return
     # msg = ' '.join(context.args)
-    os.system('spotify.sh')
+    os.system('bot/create_spotify.sh')
     
 async def spotify_stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None: 
     # if len(context.args)<1:
     #     await update.message.reply_text(f'Pero qué digo???')
     #     return
     # msg = ' '.join(context.args)
-    os.system('spotify_stop.sh')
+    os.system('sbot/potify_stop.sh')
     
 async def increase_volume(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             await update.message.reply_text(f'Subiendo volumen')
